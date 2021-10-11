6dcaf608b4ba923fc2e6c092ba9f693a7c42a22a
Andrii Bren <andriybren5@gmail.com>
2021-10-11 23:00:11 +0300

    Initial commit, create skeleton of project

---
+++ src/main/java/com/lnu/edu/ua/botnotifier/BotnotifierApplication.java
@@ -0,0 +1,13 @@
+package com.lnu.edu.ua.botnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotnotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotnotifierApplication.class, args);
+	}
+
+}
